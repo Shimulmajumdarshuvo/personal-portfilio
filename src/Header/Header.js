54d8fd9027840fca2cbb1019b033b4cc2f67a191
shimul <shimul@gmail.com>
2022-06-09 11:23:46 +0600

    navbar section added

---
+++ src/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+            <Navbar className='navbar-part' collapseOnSelect expand="lg" sticky='top' >
+                <Container className='nav-bar'>
+                    <Navbar.Brand as={Link} to="/">
+                        <img width={130} src="https://uncw.edu/eportfolio/images/ePortfolio_510x247.png" alt="" />
+
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+
+                            <Nav.Link href='#about'>About Me</Nav.Link>
+                            <Nav.Link href='#skill'>Skill</Nav.Link>
+                            <Nav.Link href="experience">Experience</Nav.Link>
+                            <Nav.Link href="blog">Education</Nav.Link>
+
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
